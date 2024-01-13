34250101b2183a980cadfc0a7080c0a752669235
ichwansh03 <ichwansholihin03@gmail.com>
2024-01-13 10:21:54 +0700

    state composing

--- app/src/main/java/com/ichwan/learn/composeessential/basic/EssentialActivity.kt
+++ app/src/main/java/com/ichwan/learn/composeessential/basic/EssentialActivity.kt
@@ -3,9 +3,17 @@ package com.ichwan.learn.composeessential.basic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -14,23 +22,46 @@ class EssentialActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingWorld()
+            MyApp()
         }
     }
 }
 
 @Composable
-fun GreetingWorld(modifier: Modifier = Modifier){
-    Greeting(name = "Ichwan", modifier = modifier)
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: Set<String> = setOf("Ichwan","World")
+) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name, modifier)
+        }
+    }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier) {
-    Text(text = "Hello $name", modifier.padding(12.dp))
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 24.dp
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 4.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f).padding(extraPadding)) {
+                Text(text = "Welcome Back,")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show Less" else "Show More")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewGreeting(){
-    GreetingWorld()
+    MyApp()
 }
